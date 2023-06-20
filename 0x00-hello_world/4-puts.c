77ec80269062d2e1bb88d2d976e6a4411f7c3733
hi0ki <ansarimehdi518@gmail.com>
2023-06-19 18:32:04 -0700

    last

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *main - Enty point
- * 
- *Return : Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success) 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
